c41c2e3bff4956525bcbbe068bfdd2978997e761
ronin liu <11370850+gaseity@users.noreply.github.com>
2025-08-15 08:15:02 +0800

    好看一些

--- strategy/MiniDuckSimulator.go
+++ strategy/MiniDuckSimulator.go
@@ -9,15 +9,19 @@ var ducks []string = []string{
 
 func main() {
 	for _, duck := range ducks {
+
 		d := NewDuck(duck)
 		if d == nil {
 			fmt.Println("Duck cooked ", duck)
 			continue
 		}
+		fmt.Println("----")
+
 		d.Display()
 		d.Fly()
 		d.Quack()
 		d.Swim()
+
 	}
 
 }
